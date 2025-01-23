9ec5a4c7f0c1dcf352f23d70ebc45c9d0bdd3706
Lianghong Ye <yelianghong@gmail.com>
2025-01-23 21:00:33 +0800

    change plot

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
 
 
 
-
+# parity order
     n = 5
     parity = parity_benchmark(step_val, n)
     n_data = time.size // parity.size
@@ -47,24 +47,21 @@ def main():
     print("n_step", n_step)
     print("parity size", parity.size)
     parity_plot = np.repeat(parity, n_data)
-    #parity_plot *= 1e-25
+    parity_plot = parity_plot*0.5 - 1
     print(parity_plot.size)
 
-    # plot
-    plt.figure()
-    plt.plot(time, envelope)
-    plt.plot(time, parity_plot)
-    plt.show()
+
+
 
 
 
 
     # create data set
-    n_node = n_data // 100
+    n_node = n_data // 50
     x_data = np.zeros((n_step, n_node))
     y_data = np.zeros(n_step)
 
-    points = envelope[::100]
+    points = envelope[::50]
     for i in range(n_step):
         x_data[i] = points[i*n_node:(i+1)*n_node]
         y_data[i] = parity[i]
@@ -75,7 +72,7 @@ def main():
     # x_data = x_data[10:190,:]
     # y_onehot = y_onehot[10:190,:]
     dataset = tf.data.Dataset.from_tensor_slices((x_data, y_onehot))
-    batch_size = 10
+    batch_size = 50
     dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
     print("xshape",x_data.shape)
@@ -86,11 +83,28 @@ def main():
 
     # create model
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(2, input_shape=(n_node,), activation='softmax'),
+        tf.keras.layers.Dense(2, input_shape=(n_node,), activation='sigmoid'),
     ])
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     model.fit(dataset, epochs=1000)
+
+    predictions = model.predict(x_data)
+
+    predictions = predictions[:,0]
+    predictions = predictions * 0.5 - 1
+    predictions = np.repeat(predictions, n_data)
+
+    # plot
+    plt.figure()
+    # plt.plot(time[:len(time)//2], envelope[:len(envelope)//2])
+    # plt.plot(time[:len(time)//2], parity_plot[:len(parity_plot)//2])
+    # plt.plot(time[:len(time)//2],predictions[:len(predictions)//2])
+
+    plt.plot(time, envelope)
+    plt.plot(time, parity_plot)
+    plt.plot(time, predictions)
+    plt.show()
 
 if __name__ == '__main__':
     main()
